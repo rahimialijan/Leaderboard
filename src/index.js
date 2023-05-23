3ad56b375afd143bdfeb7437514154392d29b09b
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-05-23 21:31:14 +0500

    Completion of first part of the leaderboard project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import Score from "./modules/main";
+import './style.css';
+import UI from './modules/ui';
+import Score from './modules/main';
+import Store from './modules/store';
 
-console.log (Score)
+
+//Event for display the score on the page.
+document.addEventListener('DOMContentLoaded', ()=>{
+    UI.displayScores();
+    UI.addBackgroundToList();
+});
+
+// submit Score to leaderboard 
+document.querySelector ('.submit-score').addEventListener('submit', (e)=>{
+    const name=document.querySelector('#name-input').value;
+    const score=document.querySelector('#score-input').value;
+    // prevent defluat submit action
+    e.preventDefault();
+    if (name !=='' && score !==''){
+     //Istantiate to score class
+    const scores= new Score(name, score);
+    //add score to leaderboard
+    UI.addScoreToBoard(scores);
+    
+    //add scores to localstorage
+    Store.addScore(scores)
+   
+    //Clear the input fields
+    UI.clearAllField();
+    }
+
+})
---
+++ src/modules/UI.js
@@ -0,0 +1,38 @@
+import Store from "./store";
+
+
+class UI{
+    static displayScores(){
+        const scores = Store.getScore();
+    scores.forEach((score)=> UI.addScoreToBoard(score));
+    }
+
+    static addScoreToBoard(score){
+        const ul=document.querySelector('.submited-scores');
+        const li=document.createElement('li');
+        li.classList.add('name-score');
+        li.innerHTML= `
+        <span>${score.name}</span>
+        <span>${score.score}</span>
+        `;
+        ul.appendChild(li);
+    }
+
+    static clearAllField(){
+        document.getElementById('name-input').value='';
+        document.getElementById('score-input').value='';
+    }
+
+    static addBackgroundToList(){
+        const scoreList=document.querySelectorAll('.name-score');
+         for (let i=0; i<scoreList.length; i++){
+            if (i % 2===0){
+                scoreList[i].classList.add('odd');
+            }else{
+                scoreList[i].classList.add('even');
+            }
+         }
+    }
+};
+
+export default UI;
